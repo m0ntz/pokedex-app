81583984988e25ed2ecea868a14b2ff23ad12fc5
m0ntz <109100764+m0ntz@users.noreply.github.com>
2022-09-06 11:57:33 +0200

    Added script.js file

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+let pokemonList = [
+  {
+    name: "Bulbasaur",
+    height: 0.7,
+    types: ["grass", "poisson"],
+  },
+  {
+    name: "Charmander",
+    height: 0.6,
+    types: ["fire", "flying"],
+  },
+  {
+    name: "Jigglypuff",
+    height: 0.5,
+    types: ["psychic", "poison"],
+  },
+];
