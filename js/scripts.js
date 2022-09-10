4066c375ecfaf398bde6ebc77d2cfa483e7286f3
m0ntz <109100764+m0ntz@users.noreply.github.com>
2022-09-10 11:42:43 +0200

    Replaced for loop with forEach()

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,46 @@
 // Array of pokemons
-
-let pokemonList = [
-  {
-    name: "Bulbasaur",
-    height: 0.7,
-    types: ["grass", "poisson"],
-  },
-  {
-    name: "Charmander",
-    height: 0.6,
-    types: ["fire", "flying"],
-  },
-  {
-    name: "Jigglypuff",
-    height: 0.5,
-    types: ["psychic", "poison"],
-  },
-  {
-    name: "Togepi",
-    height: 0.3,
-    types: ["fairy", "psychic"],
-  },
-];
+let pokemonRepository = function () {
+  let pokemonList = [
+    {
+      name: "Bulbasaur",
+      height: 0.7,
+      types: ["grass", "poisson"],
+    },
+    {
+      name: "Charmander",
+      height: 0.6,
+      types: ["fire", "flying"],
+    },
+    {
+      name: "Jigglypuff",
+      height: 0.5,
+      types: ["psychic", "poison"],
+    },
+    {
+      name: "Togepi",
+      height: 0.3,
+      types: ["fairy", "psychic"],
+    },
+  ];
+};
 
 // show each pokemon with its height
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 0.6) {
+
+pokemonList.forEach(function (pokemon) {
+  if (pokemon.height > 0.6) {
     document.write(
       "<li>" +
-        pokemonList[i].name +
+        pokemon.name +
         " " +
         "(height: " +
-        pokemonList[i].height +
+        pokemon.height +
         ")" +
         " - Wow, that's big!" +
         "</li>"
     );
   } else {
     document.write(
-      "<li>" +
-        pokemonList[i].name +
-        " " +
-        "(height: " +
-        pokemonList[i].height +
-        ")" +
-        "</li>"
+      "<li>" + pokemon.name + " " + "(height: " + pokemon.height + ")" + "</li>"
     );
   }
-}
+});
